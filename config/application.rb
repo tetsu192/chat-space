2c6b002f7ba5893ecd126fc64d535ba87e5c76e1
長谷川徹二 <kmkt1995129@gamil.com>
2017-06-20 09:20:21 +0900

    deviseの日本語化

--- config/application.rb
+++ config/application.rb
@@ -7,8 +7,9 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module ChatSpace
-  class Application < Rails::Application
 
+  class Application < Rails::Application
+    config.i18n.default_locale = :ja
   	config.generators do |g|
   		g.javascript false
   		g.helper     false
