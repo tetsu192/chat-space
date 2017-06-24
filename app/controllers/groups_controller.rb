40fbf06079c8a0a24044ba48cc3ec81e8c7731df
長谷川徹二 <kmkt1995129@gamil.com>
2017-06-24 20:35:44 +0900

    グループ作成機能の実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,21 @@
 class GroupsController < ApplicationController
 
+  def index
+    @groups = Group.all
+  end
+
   def new
+    @group = Group.new
+    @group.members.build
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -11,5 +23,11 @@ class GroupsController < ApplicationController
 
   def update
   end
-  
+
+  private
+  def group_params
+    params.require(:group).permit(:name, { :user_ids=> [] })
+  end
+
+
 end
