d82810a7509292092e06a5b1ce46ccfe9b9db2ea
長谷川徹二 <kmkt1995129@gamil.com>
2017-06-24 20:38:29 +0900

    groupアクションのindexにログイン中のユーザーが所属するグループの表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages#index'
-  resources :messages , only: [:index]
+  root 'groups#index'
   resources :users , only: [:edit, :update]
-  resources :groups , only: [:new, :create, :edit, :update]
+  resources :groups , only: [:index, :new, :create, :edit, :update] do
+    resources :messages , only: [:index]
+end
 end
