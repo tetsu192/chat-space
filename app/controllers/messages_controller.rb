f4a6a624dd24d047d7ad111fa074d095e6da428a
長谷川徹二 <kmkt1995129@gamil.com>
2017-06-28 13:24:15 +0900

    messageのindex画面でグループ名、参加ユーザー名を表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,13 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @messages = Message.all
+    @group = Group.find(params[:group_id])
+    @members = @group.members
+    @groups = current_user.groups
+  end
+
+  def create
   end
 
 end
