8dd65ca6e60e8aa3b6d964e8bb0e030d83bddb04
長谷川徹二 <kmkt1995129@gamil.com>
2017-06-29 19:01:13 +0900

    指摘箇所の修正
    render, partial, collection

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,24 +1,32 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
 
-  def index
-    @message = Message.new
+  def variable
     @group = Group.find(params[:group_id])
+    @groups = current_user.groups
+    @message = Message.new
     @messages = @group.messages
     @members = @group.members
-    @groups = current_user.groups
+  end
+
+  def index
+    variable
   end
 
   def create
     @message = Message.new(text: message_params[:text], image: message_params[:image], user_id: current_user.id, group_id: params[:group_id])
     if @message.save
-      redirect_to group_messages_url
+      variable
+      render :index
     else
-      redirect_to group_messages_url, alert: 'メッセージを入力してください'
+      variable
+      flash.now[:alert] = 'メッセージを入力してください'
+      render :index
     end
   end
 
   private
+
   def message_params
     params.require(:message).permit(:text, :image)
   end
