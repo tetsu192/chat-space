1f228c2f1283d3072db2bd0bf36e949689c8c310
長谷川徹二 <kmkt1995129@gamil.com>
2017-07-23 09:31:04 +0900

    [update] インデントの修正

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,13 +1,13 @@
 module GroupsHelper
 
   def latest_comment(a_group)
-      if a_group.messages.last.blank?
-        'メッセージがありません'
-      elsif a_group.messages.last.text.blank?
-        '画像が投稿されています'
-      else
-        a_group.messages.last.text
-      end
+    if a_group.messages.last.blank?
+      'メッセージがありません'
+    elsif a_group.messages.last.text.blank?
+      '画像が投稿されています'
+    else
+      a_group.messages.last.text
+    end
   end
 
 end
