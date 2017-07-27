daa927c89c8288e86e7a93146a7a85cc4038b078
長谷川徹二 <kmkt1995129@gamil.com>
2017-07-27 17:36:55 +0900

    [add] messageモデルのテスト

---
+++ spec/factories/messages.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+
+  factory :message do
+    text            'aaa'
+    image           Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/image.jpg'))
+    user_id         '1'
+    group_id        '1'
+  end
+
+end
---
+++ spec/models/message_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+describe Message do
+  describe '#create' do
+
+    context 'when a text exist' do
+      it 'is valid with a text' do
+        message = build(:message, image: '')
+        expect(message).to be_valid
+      end
+    end
+
+    context 'when an image exist' do
+      it 'is valid with an image' do
+        message = build(:message, text: '')
+        expect(message).to be_valid
+      end
+    end
+
+    context 'when a text and an image exist' do
+      it 'is valid with a text and an image' do
+        message = build(:message)
+        expect(message).to be_valid
+      end
+    end
+
+    context 'when a text and an image are blank' do
+      it 'is invalid without a text and an image' do
+        message = build(:message, text: '', image: '')
+        message.valid?
+        expect(message.errors[:text || :image]).to include('を入力してください。')
+      end
+    end
+
+    context 'when a group_id is blank' do
+      it 'is invalid without a group_id' do
+        message = build(:message, group_id: '')
+        message.valid?
+        expect(message.errors[:group_id]).to include('を入力してください。')
+      end
+    end
+
+    context 'when a user_id is blank' do
+      it 'is invalid without a user_id' do
+        message = build(:message, user_id: '')
+        message.valid?
+        expect(message.errors[:user_id]).to include('を入力してください。')
+      end
+    end
+
+  end
+end
