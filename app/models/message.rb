affca1a408d18930c5e119dc26d5c3bc2438dbc9
長谷川徹二 <kmkt1995129@gamil.com>
2017-07-27 20:31:44 +0900

    [update] messageモデルのテスト

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,4 @@ class Message < ApplicationRecord
   belongs_to :group
   mount_uploader :image, ImageUploader
   validates :text, presence: true, if: Proc.new{ |message| message.image.blank? }
-  validates_presence_of :user_id, :group_id
 end
