f28028849dbb798b9fc6bb4a155bc0cf1b5d621b
長谷川徹二 <kmkt1995129@gamil.com>
2017-07-27 17:51:23 +0900

    [update] 画像保存場所の変更

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
 
   factory :message do
     text            'aaa'
-    image           Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/image.jpg'))
+    image           Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/image.jpg'))
     user_id         '1'
     group_id        '1'
   end
