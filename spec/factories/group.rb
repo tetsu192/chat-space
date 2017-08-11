286b9a7049de31f93c85cde3c2078ffe2609b5a8
長谷川徹二 <kmkt1995129@gamil.com>
2017-08-11 18:23:13 +0900

    [add] ファクトリの設定　user/group/message

---
+++ spec/factories/group.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :group do
+    name  Faker::Name.name
+    created_at  { Faker::Time.between(5.days.ago, 3.days.ago, :all) }
+  end
+end
---
+++ spec/factories/user.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+  pass = Faker::Internet.password(8)
+
+  factory :user do
+    name                  Faker::Name.name
+    email                 Faker::Internet.email
+    password              pass
+    password_confirmation pass
+
+    after(:create) do |user|
+      user.groups << create(:group)
+    end
+  end
+end
