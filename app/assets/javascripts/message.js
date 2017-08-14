ca262692067baa0c4fb3bff1b609217dec51607c
長谷川徹二 <kmkt1995129@gamil.com>
2017-08-15 08:51:02 +0900

    [add] ajaxでcreateアクションを動かす

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,28 @@
 $(function() {
   $('#new_message').on('submit', function(e) {
-    console.log('aaa');
     e.preventDefault();
+    var group_id = $('.group_id').val();
+    var fd = new FormData($('#new_message').get(0));
+    fd.append('text', $('message-form__text').val());
+    console.log(fd)
+
+    $.ajax({
+      url: '/groups/27/messages.json',
+      type: 'POST',
+      dataType: 'json',
+      data: fd,
+      processData: false,
+      contentType: false
+    })
+    .done(function(data) {
+      console.log("success");
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      console.log("complete");
+    });
+
   });
 });
