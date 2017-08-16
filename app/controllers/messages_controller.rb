b535f808866038ecd3a3dd5a6185c4393d21f69d
長谷川徹二 <kmkt1995129@gamil.com>
2017-08-16 22:46:47 +0900

    [add] respond_to でhtmlとjsonの処理を分ける

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,19 +1,22 @@
 class MessagesController < ApplicationController
-  before_action :set_variables, only: [:index, :create]
 
   def index
+    @group = Group.find(params[:group_id])
+    @groups = current_user.groups
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
+    @members = @group.members.includes(:user)
   end
 
   def create
-    message = current_user.messages.new(message_params)
-    if message.save
+    @message = current_user.messages.new(message_params)
+    if @message.save
       respond_to do |format|
         format.html { redirect_to group_messages_path }
-        format.json { render json: message }
+        format.json
       end
     else
-      flash.now[:alert] = 'メッセージを入力してください'
-      render :index
+      redirect_to group_messages_path
     end
   end
 
@@ -21,14 +24,6 @@ class MessagesController < ApplicationController
 
   def message_params
     params.require(:message).permit(:text, :image, :group_id).merge(group_id: params[:group_id])
-  end
-
-  def set_variables
-    @group = Group.find(params[:group_id])
-    @groups = current_user.groups
-    @message = Message.new
-    @messages = @group.messages.includes(:user)
-    @members = @group.members.includes(:user)
   end
 
 end
