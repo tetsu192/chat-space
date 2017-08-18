7de743070ffa06fab8adc5765084d06069da2e3a
長谷川徹二 <kmkt1995129@gamil.com>
2017-08-18 10:20:58 +0900

    [add] インクリメンタルサーチの実装

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -1,5 +1,58 @@
 $(function(){
+
+//インクリメンタルサーチで一致したユーザーが表示される処理
+
+  function listUsers(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                </div>`
+    $('#user-search-result').append(html);
+  };
+
   $('#user-search-field').on('keyup', function(){
     var input = $('#user-search-field').val();
+    var href = window.location.href;
+
+    $.ajax({
+      type: 'GET',
+      url: href,
+      data: { keyword: input },
+      dataType: 'json'
+    })
+
+    .done(function(users) {
+      $('#user-search-result').empty();
+      users.forEach(function(user){
+        listUsers(user);
+      });
+    })
+    .fail(function(){
+      alert('ユーザーを表示できません');
+    })
+  });
+
+//「追加」ボタンを押したときにリストから消える処理
+
+  function addUser(user_name, user_id) {
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+                  <p class='chat-group-user__name'>${user_name}</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+    $('#chat-member').append(html);
+  };
+
+  $(document).on('click', '.user-search-add', (function(){
+    var user_name = $(this).data('user-name');
+    var user_id = $(this).data('user-id');
+    $(this).parent().remove();
+    addUser(user_name, user_id);
+  }));
+
+//「削除」ボタンを押したときにメンバーから消える処理
+
+  $(document).on('click', '.user-search-remove', function(){
+    $(this).parent().remove();
   });
 });
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  before_action :search, except: [:create, :update]
 
   def index
     @groups = current_user.groups
@@ -6,11 +7,6 @@ class GroupsController < ApplicationController
 
   def new
     @group = Group.new
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(10)
-    respond_to do |format|
-      format.html
-      format.json
-    end
   end
 
   def create
@@ -24,18 +20,16 @@ class GroupsController < ApplicationController
 
   def edit
     @group = Group.find(params[:id])
-    @members = @group.members
-    if @members.find_by(user_id: current_user.id).blank?
-      redirect_to groups_url
-    end
+    @members = @group.members.includes(:user).reject{|mem| mem.user_id == current_user.id } if @group.members.count >= 2
   end
 
   def update
     @group = Group.find(params[:id])
+    @members = @group.members.includes(:user).reject{|mem| mem.user_id == current_user.id } if @group.members.count >= 2
     if @group.update(group_params)
       redirect_to group_messages_url(@group), notice: 'グループを編集しました'
     else
-      render :edit
+      redirect_to edit_group_path(@group), flash: { error: @group.errors.full_messages }
     end
   end
 
@@ -43,6 +37,14 @@ class GroupsController < ApplicationController
 
   def group_params
     params.require(:group).permit(:name, { user_ids: [] })
+  end
+
+  def search
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(10).reject{|mem| mem.id == current_user.id }
+    respond_to do |format|
+     format.html
+     format.json { render :search, format: [:jbuilder] }
+    end
   end
 
 end
