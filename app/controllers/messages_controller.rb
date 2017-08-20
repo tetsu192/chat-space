119956992f90ecc43b729534a8d6c9b40e576a42
長谷川徹二 <kmkt1995129@gamil.com>
2017-08-20 15:02:18 +0900

    [add] respond_to で場合分け

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,18 +6,19 @@ class MessagesController < ApplicationController
     @message = Message.new
     @messages = @group.messages.includes(:user)
     @members = @group.members.includes(:user)
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def create
     @message = current_user.messages.new(message_params)
     if @message.save
       respond_to do |format|
-        format.html { redirect_to group_messages_path }
+        format.html
         format.json
       end
-    else
-      flash.now[:alert] = 'メッセージを入力してください'
-      redirect_to group_messages_path
     end
   end
 
